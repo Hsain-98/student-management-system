74dfd6e29be65c8af895924945be2e388b37b865
Hsain-98 <ahsainekhadija@gmail.com>
2022-11-19 16:06:56 +0100

    Initial commit

--- src/main/java/com/example/studentmanagementsystem/service/StudentService.java
+++ src/main/java/com/example/studentmanagementsystem/service/StudentService.java
@@ -8,6 +8,6 @@ public interface StudentService {
     List<Student> getAllStudents(); 
     Student saveStudent(Student student);
     Student getStudentById(Long id);
-    Student updateStudent(Student student);
+    Student updateStudent(Long id, Student student);
     void deleteStudent(Long id);
 }
